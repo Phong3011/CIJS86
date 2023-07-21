f0daaceaa7b9ec3330249301d28b3fbbe9f3b8f2
Phong <131786469+Phong3011@users.noreply.github.com>
2023-07-21 17:48:57 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "./components/lesson2/Layout";
+import Card from "./components/lesson2/Theme";
+
+
+const App = () => {
+  return (
+    <div style={{backgroundColor:"black"}}>
+      <Layout />
+      <Card />
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './button.css';
+const Button = () => {
+  return(
+    <div className="btn-footer">
+      <button className="btn-left">Buy Tickets</button>
+      <button className="btn-right">Get free</button>
+    </div>
+  )
+}
+
+export default Button;
